168ba9a094e1879264f61422eed1ad3152e37b24
student <redbeansodapop@hotmail.com>
2018-03-22 19:32:32 +0800

    finished lab 6

--- lab6/ecb.py
+++ lab6/ecb.py
@@ -3,13 +3,43 @@
 # Oka, SUTD, 2014
 from present import *
 import argparse
+import binascii
 
 nokeybits=80
 blocksize=64
 
 
 def ecb(infile,outfile,key,mode):
-    pass
+    inp_file = open(infile, mode = 'rb')
+    key_file = open(key, mode = 'r')
+    out_file = open(outfile, mode = 'wb')
+    print('     Openning files...')
+    key = int(key_file.read(), 16)
+    out = []
+    if mode == 'e':
+        print('     Encrypting <' + infile + '> into <' + outfile + '>...')
+    elif mode == 'd':
+        print('     Decrypting <' + infile + '> into <' + outfile + '>...')
+    else:
+        return
+
+    plainBlock = inp_file.read(8)
+    while (plainBlock != b''):
+        block = ''
+        for j in plainBlock:
+            block += format(j, '08b')
+        block = int(block, 2)
+
+        if mode == 'e':
+            out.append(binascii.unhexlify('%016x' % present(block, key)))
+        elif mode == 'd':
+            out.append(binascii.unhexlify('%016x' % present_inv(block, key)))
+
+        plainBlock = inp_file.read(8)
+
+    for byte in out:
+        out_file.write(byte)
+    print('-----DONE-----')
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser(description='Block cipher using ECB mode.')
@@ -22,6 +52,8 @@ if __name__=="__main__":
     infile=args.infile
     outfile=args.outfile
     keyfile=args.keyfile
+    mode=args.mode
 
+    ecb(infile, outfile, keyfile, mode)
 
 
--- lab6/extract.py
+++ lab6/extract.py
@@ -2,12 +2,58 @@
 # ECB plaintext extraction skeleton file for 50.020 Security
 # Oka, SUTD, 2014
 import argparse
+import operator
 
 def getInfo(headerfile):
-    pass
+    print('     Getting header file info...')
+    header = open(headerfile, mode='rb').read()
+    return header
+
+def getWhiteBytes(infile, header):
+    print('     Finding white byte...')
+    file = open(infile, mode = 'rb')
+    inp = file.read(len(header) + 1)
+    inp = file.read(8)
+    d = {}
+    while inp != b'':
+        if inp in d:
+            d[inp] += 1
+        else:
+            d[inp] = 1
+        inp = file.read(8)
+    frequence = sorted(d.items(), key = operator.itemgetter(1))
+    file.close()
+    print('     Found white byte!')
+    return frequence[-1][0]
+
 
 def extract(infile,outfile,headerfile):
-    pass
+    print('     Extracting <' + infile + '> into <' + outfile + '>...')
+    header = getInfo(headerfile)
+    whiteBytes = getWhiteBytes(infile, header)
+
+    inp_file = open(infile, mode = 'rb')
+    out = []
+
+    # skip header portion
+    inp = inp_file.read(len(header) + 1)
+
+    # start reading file
+    print('     Extracting key message...')
+    inp = inp_file.read(8)
+    while inp != b'':
+        if inp == whiteBytes:
+            out.append('00000000')
+        else:
+            out.append('11111111')
+        inp = inp_file.read(8)
+
+    out_file = open(outfile, mode = 'w')
+    out_file.write(header.decode('utf-8') + '\n')
+    for byte in out:
+        out_file.write(byte)
+
+    print('-----DONE-----')
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser(description='Extract PBM pattern.')
